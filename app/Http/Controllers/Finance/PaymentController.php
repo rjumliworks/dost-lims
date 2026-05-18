fc08d38aea2dd475d40236b77ec0c7fb9f3c78d7
kradworkz <80160702+kradworkz@users.noreply.github.com>
2026-05-18 12:35:08 +0800

    Update PaymentController.php

--- app/Http/Controllers/Finance/PaymentController.php
+++ app/Http/Controllers/Finance/PaymentController.php
@@ -140,7 +140,7 @@ class PaymentController extends Controller
 
         return response()->json([
             'status' =>
-                $data['data']
+                $data['data']['attributes']['status']
         ]);
     }
 
