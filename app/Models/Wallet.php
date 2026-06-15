a25aeba4c98a1e8476639163b95309c4e64cd1c9
kradworkz <80160702+kradworkz@users.noreply.github.com>
2026-06-15 16:03:08 +0800

    aw

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -6,5 +6,40 @@ use Illuminate\Database\Eloquent\Model;
 
 class Wallet extends Model
 {
-    //
+    protected $fillable = [
+        'total', 
+        'available',
+        'deduction',
+        'customer_id'
+    ];
+
+    public function transactions()
+    {
+        return $this->hasMany('App\Models\WalletTransaction', 'wallet_id')->orderBy('created_at','DESC');
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo('App\Models\Customer', 'customer_id', 'id');
+    }
+
+    public function setAvailableAttribute($value)
+    {
+        $this->attributes['available'] = trim(str_replace(',','',$value),'₱');
+    }
+
+    public function getAvailableAttribute($value)
+    {
+        return '₱'.number_format($value,2,'.',',');
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return date('M d, Y g:i a', strtotime($value));
+    }
+
+    public function getCreatedAtAttribute($value)
+    {
+        return date('M d, Y g:i a', strtotime($value));
+    }
 }
--- app/Models/WalletTransaction.php
+++ app/Models/WalletTransaction.php
@@ -6,5 +6,46 @@ use Illuminate\Database\Eloquent\Model;
 
 class WalletTransaction extends Model
 {
-    //
+     protected $fillable = [
+        'code',
+        'amount',
+        'balance',
+        'is_credit',
+        'wallet_id'
+    ];
+
+    public function transacable()
+    {
+        return $this->morphTo();
+    }
+
+    public function wallet()
+    {
+        return $this->belongsTo('App\Models\Wallet', 'wallet_id', 'id');
+    }
+
+    public function receipt()
+    {
+        return $this->belongsTo('App\Models\FinanceReceipt', 'receipt_id', 'id');
+    }
+
+    public function setAmountAttribute($value)
+    {
+        $this->attributes['amount'] = trim(str_replace(',','',$value),'₱');
+    }
+
+    public function getAmountAttribute($value)
+    {
+        return '₱'.number_format($value,2,'.',',');
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return date('M d, Y g:i a', strtotime($value));
+    }
+
+    public function getCreatedAtAttribute($value)
+    {
+        return date('M d, Y g:i a', strtotime($value));
+    }
 }
