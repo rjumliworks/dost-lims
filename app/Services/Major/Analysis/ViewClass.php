09cd0e16baddf6a972864551c0cc3e4c5692f50a
kradworkz <80160702+kradworkz@users.noreply.github.com>
2026-06-23 13:39:30 +0800

    Update ViewClass.php

--- app/Services/Major/Analysis/ViewClass.php
+++ app/Services/Major/Analysis/ViewClass.php
@@ -26,6 +26,7 @@ class ViewClass
                     })
                     ->exists();
                 }else{
+                    $samplenames = null;
                     $hasSampleNames = false;
                 }
                 $data = TestserviceResource::collection(
