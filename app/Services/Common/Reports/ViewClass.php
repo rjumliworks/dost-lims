080a107b6300080824d675f9625a0c97d2006c2e
kradworkz <80160702+kradworkz@users.noreply.github.com>
2026-07-14 09:28:44 +0800

    Update ViewClass.php

--- app/Services/Common/Reports/ViewClass.php
+++ app/Services/Common/Reports/ViewClass.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Common\Reports;
 
+use App\Models\ListLaboratory;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Exports\AccomplishmentExport;
 
@@ -19,7 +20,7 @@ class ViewClass
         $month = ($request->month) ? \DateTime::createFromFormat('F', $request->month)->format('m') : date('m');  
         $year = ($request->year) ? $request->year : date('Y');
         $lab = $request->laboratory;
-        $name = $request->labname;
+        $name = ListLaboratory::where('id',$request->laboratory)->value('short');
 
         return Excel::download(new AccomplishmentExport($month,$year,$lab), $name.'_'.strtolower($request->month).'_accomplishment.xlsx');
     }
