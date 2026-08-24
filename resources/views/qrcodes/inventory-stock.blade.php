706a2682287295419542ab70e2c07fb6d5ed43fc
kradworkz <80160702+kradworkz@users.noreply.github.com>
2026-08-24 10:25:34 +0800

    Update inventory-stock.blade.php

--- resources/views/qrcodes/inventory-stock.blade.php
+++ resources/views/qrcodes/inventory-stock.blade.php
@@ -3,6 +3,9 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <style>
+            @page {
+                margin: 0;
+            }
             html * {
                 font-family:Arial, Helvetica, sans-serif;
                 margin: 0;
@@ -10,23 +13,23 @@
             }
             body {
                 text-align: center;
-                padding-top: 40pt;
+                padding: 4pt 6pt 2pt 6pt;
             }
             .image {
-                width: 30pt;
-                height: 30pt;
+                width: 155pt;
+                height: 155pt;
             }
             .name {
-                font-size: 9pt;
-                font-weight: bold;
-                margin-top: 8pt;
-                padding: 0 12pt;
+                font-size: 6pt;
+                font-weight: normal;
+                margin-top: -5pt;
+                line-height: 1.15;
                 word-wrap: break-word;
             }
             .code {
                 font-size: 7pt;
                 color: #333333;
-                margin-top: 3pt;
+                margin-top: 0pt;
             }
         </style>
     </head>
