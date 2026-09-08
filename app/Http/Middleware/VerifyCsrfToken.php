fee4274eec1e7101cb9cdefc086801b7662bdc2c
kradworkz <80160702+kradworkz@users.noreply.github.com>
2026-09-08 16:07:19 +0800

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,7 +3,6 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
-use Symfony\Component\HttpFoundation\Cookie;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -15,20 +14,4 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         '/webhook/paymongo',
     ];
-
-    protected function newCookie($request, $config)
-    {
-        return new Cookie(
-            config('session.cookie') . '_xsrf',
-            $request->session()->token(),
-            $this->availableAt(60 * $config['lifetime']),
-            $config['path'],
-            $config['domain'],
-            $config['secure'],
-            false,
-            false,
-            $config['same_site'] ?? null,
-            $config['partitioned'] ?? false
-        );
-    }
 }
